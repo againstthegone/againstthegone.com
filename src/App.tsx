17ff7135d86b73ebc7a76bcc02342a50e2ebdbeb
Robert Chow <rchow@thoughtworks.com>
2021-05-18 19:55:53 +0100

    Convert to react spa

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <main className="main">
+        <h1 className="title">
+          AGAINST THE GONE
+        </h1>
+
+        <nav className="nav">
+          <a href="/#">Thoughts.</a>
+          <a href="/#">Impressions.</a>
+          <a href="/#">Code.</a>
+        </nav>
+      </main>
+
+      <footer className="footer">
+        © 2021 AGAINST THE GONE
+      </footer>
+    </div>
+  );
+}
+
+export default App;
